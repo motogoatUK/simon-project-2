68a95a177e7323be5f26b078ab7cfe67b49b0cf3
motogoatUK <software@motogoat.co.uk>
2025-05-21 00:50:55 +0100

    add inProgress boolean to game object. streamline hideFlipped routine and allow it to hide any number of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,7 @@ const game = {
     score: 0,
     misses: 0,
     turn: 0,
+    inProgress: false,
     cardsFlipped: [],
     cardsMatched: [],
 }
@@ -48,6 +49,7 @@ const game = {
 function startGame() {
     initCards();
     addCardListeners() ? console.log("card listeners started") : console.log("card listener failed to start");
+    game.inProgress = true;
     // the event listeners now handle the rest of game
 }
 
@@ -55,7 +57,7 @@ function addCardListeners() {
     /* Add event handlers for card clicks
      using once property to prevent same card clicking */
     [...cardElements].forEach((card, i) => {
-        card.addEventListener("click", () => { showCard(i) }, { once: true });
+        card.addEventListener("click", () => showCard(i), { once: true });
     });
     return true;
 }
@@ -74,23 +76,21 @@ function initCards() {
         game.cards[i + 1] = cardValues.at(i);
         /* extra i++ to skip 2 */
         i++;
-    }
+    };
     console.log(game.cards);
-}
+};
 
 function showCard(num) {
-    // check card id not already flipped
-    /* not required as eventlistener will only fire once when card is initally clicked on */
-    /* check that less than two cards are already flipped if not then ignore the last click and re add eventlistener for it */
-    if (game.cardsFlipped.length < 2) {
+    /* check if game is ready for selection - if not then ignore the last click and re add eventlistener for it */
+    if (game.inProgress) {
+
         thisCard = cardElements[num];
         thisCard.classList.add("revealed");
         thisCard.innerHTML = game.cards[num];
         /* add to cardsFlipped, push() returns the new length of array so we can check directly */
         if (game.cardsFlipped.push(num) > 1) {
-            /* We should prevent any more cards being clicked.
-            remove click handlers?
-            /* 
+            /* We should prevent any more cards being selected.*/
+            game.inProgress = false; //no card clicks accepted until all cards shown have been flipped back or removed from the board
             /* check for match
             We need a delay here (set timeout) to give the user time to see the card before checking */
             setTimeout(() => checkMatch(), 400);
@@ -99,8 +99,8 @@ function showCard(num) {
         notify("stop trying to cheat me");
         setTimeout(cardElements[num].addEventListener("click", () => { showCard(num) }, { once: true }), 200);
 
-    }
-}
+    };
+};
 function checkMatch() {
     /*There should only ever be 2 cards in cardsFlipped array. If not show an error */
     if (game.cardsFlipped.length !== 2) {
@@ -124,25 +124,26 @@ function checkMatch() {
 };
 
 function hideFlipped() {
-    /*This routine should only ever run if there at least 2 cards in cardsFlipped array.*/
-    if (game.cardsFlipped.length < 2) {
-        console.error("Less than 2 cards in flipped array!");
+    /* There should only ever be at most 2 cards in cardsFlipped array */
+    /* As this function will run any number of flips even if only 1 card is showing
+     (as sometimes in the case of a reset game), we will check and log an error if there are more than 2 */
+    let numFlipped = game.cardsFlipped.length;
+    if (numFlipped > 2) {
+        console.error("More than 2 cards in flipped array!");
     } else {
-        let last = game.cardsFlipped.pop(); //remove from flipped array
-        let first = game.cardsFlipped.pop();
-        let lastCard = cardElements[last]; // get the relevant HTML elements 
-        let firstCard = cardElements[first];
+        for (let i = 0; i < numFlipped; i++) {
 
-        setTimeout(() => {
-            firstCard.innerHTML = parseInt(first) + 1; // add 1 as cards are numbered 1-16 and array is 0-15
-            firstCard.classList.remove("revealed"); // remove revealed class
-            firstCard.addEventListener("click", () => { showCard(first) }, { once: true }); // add back the event listener
-            lastCard.innerHTML = parseInt(last) + 1;
-            lastCard.classList.remove("revealed");
-            lastCard.addEventListener("click", () => { showCard(last) }, { once: true });
-        }, 1000);
-    }
-}
+            let last = game.cardsFlipped.pop(); //remove from flipped array
+            let lastCard = cardElements[last]; // get the relevant HTML element 
+            setTimeout(() => {
+                lastCard.innerHTML = parseInt(last) + 1; // add 1 as cards are numbered 1-16 and array is 0-15
+                lastCard.classList.remove("revealed"); // remove revealed class
+                game.inProgress = true;
+                lastCard.addEventListener("click", () => { showCard(last) }, { once: true });// add back the event listener
+            }, 1000);
+        };
+    };
+};
 
 function notify(message) {
     const element = document.getElementById("notification");
@@ -154,16 +155,16 @@ function notify(message) {
         element.style.removeProperty("display");
         tableTop.style.removeProperty("opacity");
     }, 1000);
-}
+};
 function controlButtonClicked() {
     if (btnStart.innerText === "Start") {
         btnStart.innerText = "Reset Game";
     } else {
-       // hide any cards that have been flipped
-       // remove event listeners
+        // hide any cards that have been flipped
+        // remove event listeners
         //reset data tbc
-       
+
         btnStart.innerText = "Start";
-    }
+    };
     startGame();
-}
+};
